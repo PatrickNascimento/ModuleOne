f33dabdc981e2e838d7291ea3b6b630e5f21f366
Patrick <psn1462@gmail.com>
2022-06-27 11:38:57 -0300

    feat: modulo_microAPP
    Modulo MicroAPP

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Modulo MicroAPP Carregado com sucesso!
+      </Text>
+    
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',  
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
